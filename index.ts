ff37826684c01b002ab556508c77c1b53f3dfaf6
Nikita Ulko <ulkenauer@gmail.com>
2021-08-10 18:57:34 +0700

    upd

--- index.ts
+++ index.ts
@@ -2,20 +2,36 @@ import axios from 'axios';
 
 import { SeoMetadata } from './src/utils'
 
-import { extractSeoMetadataFromPage, extractUrlsFromSitemap } from'./src/utils'
-
+import { extractSeoMetadataFromPage, extractUrlsFromSitemap } from './src/utils'
+import transform from 'stream-transform'
+import stringify from 'csv-stringify'
+import { chunk, take } from 'lodash';
 const main = async (): Promise<void> => {
-    const sitemap : string[] = extractUrlsFromSitemap((await axios.get('https://ersh.su/sitemap.xml')).data)
+    const sitemapChunks: string[][] = chunk(extractUrlsFromSitemap((await axios.get('https://ersh.su/sitemap.xml')).data), 5)
+
+    let result: { url: string, seo: SeoMetadata }[] = []
+
+    for (let sitemapChunk of sitemapChunks) {
+        for (let url of sitemapChunk) {
+            while(true) {
+                try {
+                    const page = (await axios.get(url)).data
+                    const seo = extractSeoMetadataFromPage(page)
+                    result.push({ seo, url })
+                    break;
+                } catch(err) {
     
-    for (let url of sitemap) {
-        const page = (await axios.get(url)).data
-        const metadata = extractSeoMetadataFromPage(page)
-        console.log(url);
-        
-        console.log(metadata);
-        
+                }
+            }
+        }
+        transform(result, (data) => {
+            return { url: data.url, ...data.seo };
+        }).pipe(
+            stringify({ columns: { url: 'url', title: 'title', description: 'description', keywords: 'keywords' }, header: true })
+        ).pipe(process.stdout)
     }
-    
+
+
 }
 
 main()
